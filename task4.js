1dc4e58cc923f07c5992d9bc324ef398d07dfd73
Eileen Li <eileenlimur@gmail.com>
2022-02-05 12:58:37 -0500

    Implement get country capital

--- task4.js
+++ task4.js
@@ -1,9 +1,12 @@
-// Task 4: get the neigher countries of Columbia
+// Task 4: get the capital of Columbia
+
+const process = { env: { COUNTRYLAYER_API_KEY: null } };
+process.env.COUNTRYLAYER_API_KEY = "c7bf493cfd636e6ecf8318f0b0d4e249";
 
 const fetchCountry = async (alpha3Code) => {
   try {
     const res = await fetch(
-      `https://restcountries.eu/rest/v2/alpha/${alpha3Code}`
+      `http://api.countrylayer.com/v2/alpha/${alpha3Code}?access_key=${process.env.COUNTRYLAYER_API_KEY}`
     );
 
     const data = await res.json();
@@ -13,3 +16,21 @@ const fetchCountry = async (alpha3Code) => {
     console.log(error);
   }
 };
+
+const fetchCountryCapital = async () => {
+  const columbia = await fetchCountry('col');
+
+  console.log(columbia);
+
+  const capital = columbia.capital;
+
+  console.log(capital);
+
+  //original assignment (can't be done due to API changes);
+  // const neighbors = await Promise.all(
+  //   columbia.borders.map(border => fetchCountry(border))
+  // );
+  // console.log(neighbors);
+}
+
+fetchCountryCapital();
