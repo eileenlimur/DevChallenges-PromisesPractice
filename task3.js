18b9cef404ed0d848b33fe7db0978eadbe27a5a2
Eileen Li <eileenlimur@gmail.com>
2022-02-05 12:48:48 -0500

    Implement country fetch

--- task3.js
+++ task3.js
@@ -1,2 +1,15 @@
 // Task 3: using https://restcountries.eu/ API,
 // get country where alpha3Code = col
+
+const process = { env: { COUNTRYLAYER_API_KEY: null } };
+process.env.COUNTRYLAYER_API_KEY = "";
+
+const fetchData = async () => {
+  const res = await fetch(`http://api.countrylayer.com/v2/alpha/col?access_key=${process.env.COUNTRYLAYER_API_KEY}`);
+
+  const country = await res.json();
+  
+  console.log(country);
+}
+
+fetchData();
